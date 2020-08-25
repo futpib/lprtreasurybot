32648570cba950936abfa451360c1f884fffc4b1
futpib <futpib@gmail.com>
2020-08-25 14:38:22 +0300

    Switch to dedcated balance table

--- tgbot/bot/modules/status/Status.py
+++ tgbot/bot/modules/status/Status.py
@@ -4,7 +4,7 @@ from defs import google_sheets_values, beauty_sum
 
 @dp.message_handler(commands=['status'])
 async def status(message: types.Message):
-    resp = await google_sheets_values("Состояние в рублях", "A2", "A2")
+    resp = await google_sheets_values('lprtreasurybot.balance', 'A1', 'A1')
 
-    string = 'Состояние казны: \n<b>' + beauty_sum(resp_json[0][0]) + '</b> ₽'
+    string = 'Состояние казны: \n<b>' + beauty_sum(resp[0][0]) + '</b> ₽'
     await message.reply(string, parse_mode='HTML')
